4acf30f9d937c9ca8bebfde863098c4b859eaa9f
surace7523 <43046845+surace7523@users.noreply.github.com>
2021-01-23 19:48:25 -0600

    Add files via upload

---
+++ spotify.js
@@ -0,0 +1,41 @@
+
+// https://developer.spotify.com/documentation/web-playback-sdk/quick-start/#
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+// Replace with your app's client ID, redirect URI and desired scopes
+
+
+const redirectUri = "http://localhost:3000";
+const clientId = "ff1270e4ad434e938d966ac8cfb430b7"
+
+
+
+const scopes = [
+    "user-read-currently-playing",
+    "user-read-recently-played",
+    "user-read-playback-state",
+    "user-top-read",
+    "user-modify-playback-state",
+];
+
+
+
+
+export const getTokenFromUrl = () => {
+    return window.location.hash
+        .substring(1)
+        .split("&")
+        .reduce((initial, item) => {
+            var parts = item.split("=");
+            initial[parts[0]] = decodeURIComponent(parts[1]);
+
+            return initial;
+        }, {});
+};
+
+
+
+
+
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+    "%20"
+)}&response_type=token&show_dialog=true`;
